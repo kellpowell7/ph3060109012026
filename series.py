06ac0bee8b72bc19bcb970fb5e5870948f510302
Keller Powell <kbp0020@uah.edu>
2026-09-01 04:13:05 +0000

    08312026: harmonic&boas1.13.4

--- series.py
+++ series.py
@@ -1,20 +1,79 @@
 """Student assignment implementation file for Infinite Series and Taylor Series expansions."""
 
+import numpy as np
+import math
 
 # --- Student Assignment --- #
 # --- Boas --- #
 # Harmonic Series
-def harmonic(n_terms):
-    """Compute the sum of the first n terms of the harmonic series."""
-    raise NotImplementedError("Student assignment not yet implemented.")
+def harmonic(n_terms: int):
+    """
+    Compute the sum of the first n terms of the harmonic series.
+    
+    Parameters
+    ----------
+    n_terms : int
+        How many iterations of the summation will be run.
+         
+    Returns
+    -------
+    sum : float
+        The nth partial sum of the harmonic series.
+    """
+    if n_terms <= 1000:
+        sum = 0
+        for i in range(1, n_terms +1):
+            sum += (1 / i)
+    else:
+        sum = math.log(n_terms) + np.euler_gamma
 
 
 # Boas, 3rd Edition, Equation 1.13.4
 def boas_1_13_4(
     x: float, rel_tol: float = 1e-8, max_iter: int = 100
 ) -> tuple[float, int]:
-    """Compute the series of ln(1 + x)."""
-    raise NotImplementedError("Student assignment not yet implemented.")
+    """
+    Compute the series of ln(1 + x).
+    
+    Parameters
+    ----------
+    
+    x : float
+        Value to be computed about.
+    rel_tol : float
+        The relative tolerance of the specific computation.
+    max_iter : int
+        The maximum amount of iterations to be carried out. 
+        
+    Returns
+    -------
+    sum : float
+        The final approximation of the function.
+    i : int
+        The complete amount of iterations taken for the computation.
+    """
+    
+    sum = 0
+    
+    taylor = lambda x, n: (-1)**(n - 1) * x ** n / n
+    div_taylor = lambda x, n: (-1)**(n - 1) / (n * x**n)
+    
+    if abs(x) <= 1:
+        for i in range(1, max_iter + 1):
+            sum += taylor(x, i)
+        
+            if abs(sum - sum - taylor(x, i) / abs(sum)) < rel_tol: break
+            if i > max_iter: break
+        return sum, i
+    
+    if abs(x) > 1:
+        sum = np.log(x)
+        for i in range(1, max_iter + 1):
+            sum += div_taylor(x, i)
+            
+            if abs(sum - sum - taylor(x, i) / abs(sum)) < rel_tol: break
+            if i > max_iter: break
+        return sum, i
 
 
 # Boas, Problem 1.13.22
